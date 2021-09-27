a9c16433f828447baa1dcccc62fdeb252d4320ea
rased100 <rased100@gmail.com>
2021-09-27 22:24:55 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const db = {};
+
+const addToDb = item => {
+    const db = getDb();
+    if (item in db) {
+        db[item] = db[item] + 1;
+    }
+    else {
+        db[item] = 1;
+    }
+
+    saveToDb(db);
+}
+
+const removeFromDb = item => {
+    const db = getDb();
+    delete db[item];
+    saveToDb(db);
+}
+
+const saveToDb = db => {
+    const dbJSON = JSON.stringify(db);
+    localStorage.setItem('shopping-cart', dbJSON)
+}
+
+const getDb = () => {
+    let savedDb = localStorage.getItem('shopping-cart');
+    savedDb = JSON.parse(savedDb);
+    return savedDb;
+}
